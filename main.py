9468d5913f68f472c916f2baa5229ceeda5f320d
Tatev <tatevikyeghiazaryann5@gmail.com>
2025-05-02 17:33:09 +0400

    Project start

---
+++ main.py
@@ -0,0 +1,19 @@
+from database import engine
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from fastapi import FastAPI
+
+
+# Base.metadata.create_all(bind=engine)
+conn = psycopg2.connect(
+    host="localhost",
+    port=5432,
+    user="postgres",
+    password="password",
+    database="online_store",
+    cursor_factory=RealDictCursor
+    )
+
+cursor = conn.cursor()
+
+app = FastAPI()
