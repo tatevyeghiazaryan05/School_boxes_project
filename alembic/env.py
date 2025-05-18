f1bf96f37d06578fdffc3c47d10697daa9eb61b0
Tatev <tatevikyeghiazaryann5@gmail.com>
2025-05-18 15:44:58 +0400

    All tables created

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,8 @@
 from logging.config import fileConfig
-from models import Base
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
+from models import Base
 from alembic import context
 
 # this is the Alembic Config object, which provides
--- models.py
+++ models.py
@@ -72,3 +72,12 @@ class ChangePasswordCode(Base):
     code = Column(String, nullable=False)
     email = Column(String, nullable=False)
     created_at = Column(TIMESTAMP, nullable=False, server_default=text("now()"))
+
+
+class Boss(Base):
+    __tablename__ = "boss"
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP, nullable=False, server_default=text("now()"))
